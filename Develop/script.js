96960a4c6513d6a68307ca0f5f6803f05245bd69
Justin <walkerj90@icloud.com>
2021-01-12 23:43:50 -0800

    fixed special characters and uppercase

--- Develop/script.js
+++ Develop/script.js
@@ -19,6 +19,7 @@ function generatePassword() {
   var specialChars = prompt("Do you want to use special characters? Y/N");
 
   var upperCase = prompt("DO YOU WANT TO USE UPPER CASE LETTERS? Y/N");
+
   var numbersChar = prompt("Do you want numbers in your password? Y/N");
 
   var lowerCase = prompt("do you want to use lower case letters? Y/N");
@@ -26,6 +27,7 @@ function generatePassword() {
   var numbersChar = ['1234567890'.split('')];
   var lowerCase = ['abcdefghijklmnopqrstuvwxyz'.split('')]
   var upperCase = ['abcdefghijklmnopqrstuvwxyz'.toUpperCase.split('')];
+  var specialChars = [" !\"#$%&'()*+,-./:;<=>?@^_`{|}~\\"]
   var passwordGeneration = [specialChars, upperCaseGen, numbersChar, lowerCase];
 
   console.log('numbersChar');
